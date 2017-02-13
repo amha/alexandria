b59eeba0ba3b7e518d7b14785fffb8a17757658c
Amha Mogus <amha.mogus@gmail.com>
2017-02-13 20:47:48 +0300

    Replaced old navigation component with the NavigationView component and replaced actionbar with toolbar.

--- app/src/main/java/it/jaschke/alexandria/MainActivity.java
+++ app/src/main/java/it/jaschke/alexandria/MainActivity.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2017 Amha Mogus. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package it.jaschke.alexandria;
 
 import android.content.BroadcastReceiver;
@@ -6,42 +22,29 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.content.LocalBroadcastManager;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 
-public class MainActivity extends ActionBarActivity {
-
-    /**
-     * Fragment managing the behaviors, interactions and
-     * presentation of the navigation drawer.
-     */
-    // private NavigationDrawerFragment navigationDrawerFragment;
-
-    /**
-     * Used to store the last screen title. For use
-     * in {@link #restoreActionBar()}.
-     */
-    private CharSequence title;
-
-    String[] navigationMenuItems = {"My Books", "Add Book", "About"};
+public class MainActivity extends AppCompatActivity {
 
     public static boolean IS_TABLET = false;
-    private BroadcastReceiver messageReciever;
-
+    private CharSequence title;
     public static final String MESSAGE_EVENT = "MESSAGE_EVENT";
     public static final String MESSAGE_KEY = "MESSAGE_EXTRA";
 
@@ -49,169 +52,90 @@ public class MainActivity extends ActionBarActivity {
     ListOfBooks listOfBooksFragment;
     About aboutFragment;
 
-    @BindView(R.id.navigation_menu)
-    ListView navigationList;
+    private BroadcastReceiver messageReciever;
+    ActionBarDrawerToggle toolbarToggle;
 
     @BindView(R.id.drawer_layout)
     DrawerLayout mDrawerLayout;
+
+    @BindView(R.id.navigation_view)
+    NavigationView navigationView;
+
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if (savedInstanceState != null) {
-            restoreActionBar();
-        } else {
-            title = getTitle();
-        }
-
-        IS_TABLET = isTablet();
-        if (IS_TABLET) {
+        if (isTablet()) {
             setContentView(R.layout.activity_main_tablet);
         } else {
             setContentView(R.layout.activity_main);
         }
+
+        // Setup butter knife for efficient data binding
         ButterKnife.bind(this);
+
+        // Setup navigation view, the toolbar, and their corresponding listeners
+        setSupportActionBar(mToolbar);
+        toolbarToggle = toggleHelper();
+        mDrawerLayout.addDrawerListener(toolbarToggle);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        navigationUpdate(item);
+                        return false;
+                    }
+                });
 
         messageReciever = new MessageReciever();
         IntentFilter filter = new IntentFilter(MESSAGE_EVENT);
         LocalBroadcastManager.getInstance(this).registerReceiver(messageReciever, filter);
-
-        navigationList.setAdapter(new ArrayAdapter<String>(
-                this, android.R.layout.simple_list_item_1, navigationMenuItems));
-
-//        navigationDrawerFragment = (NavigationDrawerFragment)
-//                getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
-//
-//        // Set up the drawer.
-//        navigationDrawerFragment.setUp(R.id.navigation_drawer,
-//                (DrawerLayout) findViewById(R.id.drawer_layout));
     }
 
-//    @Override
-//    public void onNavigationDrawerItemSelected(int position) {
-//
-//        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-//        //Fragment nextFragment;
-//
-//        switch (position) {
-//            default:
-//            case 0:
-//                listOfBooksFragment = new ListOfBooks();
-//                transaction
-//                        .replace(R.id.container, listOfBooksFragment, "book list")
-//                        .addToBackStack((String) title);
-//                break;
-//            case 1:
-//                addBookFragment = new AddBook();
-//                transaction
-//                        .replace(R.id.container, addBookFragment, "add books")
-//                        .addToBackStack((String) title);
-//
-//                break;
-//            case 2:
-//                aboutFragment = new About();
-//                transaction
-//                        .replace(R.id.container, aboutFragment, "about")
-//                        .addToBackStack((String) title);
-//                break;
-//        }
-//        transaction.commit();
-//    }
-
-    public void setTitle(int titleId) {
-        title = getString(titleId);
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        toolbarToggle.syncState();
     }
 
-    public void restoreActionBar() {
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
-        actionBar.setDisplayShowTitleEnabled(true);
-        actionBar.setTitle(title);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
     }
-
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        if (!navigationDrawerFragment.isDrawerOpen()) {
-//            // Only show items in the action bar relevant to this screen
-//            // if the drawer is not showing. Otherwise, let the drawer
-//            // decide what to show in the action bar.
-//            getMenuInflater().inflate(R.menu.main, menu);
-//            restoreActionBar();
-//            return true;
-//        }
-//        return super.onCreateOptionsMenu(menu);
-//    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        if (id == R.id.action_settings) {
-            startActivity(new Intent(this, SettingsActivity.class));
-            return true;
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                startActivity(new Intent(this, SettingsActivity.class));
+                return true;
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+            default:
+                if (toolbarToggle.onOptionsItemSelected(item)) {
+                    return true;
+                }
         }
-
         return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    protected void onDestroy() {
-        LocalBroadcastManager.getInstance(this).unregisterReceiver(messageReciever);
-        super.onDestroy();
-    }
-
-//    @Override
-//    public void onItemSelected(String ean) {
-//        Bundle args = new Bundle();
-//        args.putString(BookDetail.EAN_KEY, ean);
-//
-//        BookDetail fragment = new BookDetail();
-//        fragment.setArguments(args);
-//
-//        int id = R.id.container;
-//        if (findViewById(R.id.right_container) != null) {
-//            id = R.id.right_container;
-//        }
-//        getSupportFragmentManager().beginTransaction()
-//                .replace(id, fragment)
-//                .addToBackStack("Book Detail")
-//                .commit();
-//
-//    }
-
-    private class MessageReciever extends BroadcastReceiver {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            if (intent.getStringExtra(MESSAGE_KEY) != null) {
-                Toast.makeText(MainActivity.this, intent.getStringExtra(MESSAGE_KEY), Toast.LENGTH_LONG).show();
-            }
-        }
-    }
-
-    public void goBack(View view) {
-        getSupportFragmentManager().popBackStack();
-    }
-
-    private boolean isTablet() {
-        return (getApplicationContext().getResources().getConfiguration().screenLayout
-                & Configuration.SCREENLAYOUT_SIZE_MASK)
-                >= Configuration.SCREENLAYOUT_SIZE_LARGE;
-    }
-
-    private class DrawerItemClickListener implements ListView.OnItemClickListener {
-        @Override
-        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            //selectItem(position);
-        }
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Pass any configuration change to the drawer toggles
+        toolbarToggle.onConfigurationChanged(newConfig);
     }
 
     @Override
@@ -222,29 +146,11 @@ public class MainActivity extends ActionBarActivity {
         super.onBackPressed();
     }
 
-//    /**
-//     * Handle tapping the FAB.
-//     */
-//    @Override
-//    public void addNewBook() {
-//
-//        // Check is Add Book fragment has been created or not
-//        if (getSupportFragmentManager().findFragmentByTag("add books") != null) {
-//            addBookFragment = (AddBook) getSupportFragmentManager()
-//                    .findFragmentByTag("add books");
-//        } else {
-//            addBookFragment = new AddBook();
-//        }
-//
-//        // Updated selected value of Navigation Drawer
-//        //navigationDrawerFragment.selectItem(1);
-//
-//        // Load Add/Scan book fragment
-////        getSupportFragmentManager().beginTransaction()
-////                .replace(R.id.container, addBookFragment, "add books")
-////                .addToBackStack((String) title)
-////                .commit();
-//    }
+    @Override
+    protected void onDestroy() {
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(messageReciever);
+        super.onDestroy();
+    }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -257,4 +163,63 @@ public class MainActivity extends ActionBarActivity {
             Log.d("AMHA", "Fragment is Null");
         }
     }
+
+    // Helper method that loads a fragment that corresponds to
+    // a navigation menu item click
+    private void navigationUpdate(MenuItem menuItem) {
+
+        // Preparing to swap out the current fragment
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        // Map the selected menu item to the relevant fragment
+        switch (menuItem.getItemId()) {
+            default:
+            case R.id.MyBooks:
+                listOfBooksFragment = new ListOfBooks();
+                fragmentManager.beginTransaction()
+                        .replace(R.id.container, listOfBooksFragment, "book list")
+                        .addToBackStack((String) title).commit();
+                break;
+            case R.id.AddBook:
+                addBookFragment = new AddBook();
+                fragmentManager.beginTransaction()
+                        .replace(R.id.container, addBookFragment, "add books")
+                        .addToBackStack((String) title).commit();
+                break;
+            case 2:
+                aboutFragment = new About();
+                fragmentManager.beginTransaction()
+                        .replace(R.id.container, aboutFragment, "about")
+                        .addToBackStack((String) title).commit();
+                break;
+        }
+        menuItem.setChecked(true);
+        setTitle(menuItem.getTitle());
+        mDrawerLayout.closeDrawers();
+    }
+
+    // Setup the toolbar toggle to animate the menu icon
+    private ActionBarDrawerToggle toggleHelper() {
+        return new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.about, R.string.books);
+    }
+
+    public void setTitle(int titleId) {
+        title = getString(titleId);
+    }
+
+    private class MessageReciever extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getStringExtra(MESSAGE_KEY) != null) {
+                Toast.makeText(MainActivity.this, intent.getStringExtra(MESSAGE_KEY), Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private boolean isTablet() {
+        return (getApplicationContext().getResources().getConfiguration().screenLayout
+                & Configuration.SCREENLAYOUT_SIZE_MASK)
+                >= Configuration.SCREENLAYOUT_SIZE_LARGE;
+    }
+
 }
